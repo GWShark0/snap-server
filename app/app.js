a7e6d82833a850c13a4278792aae8e19f8ae4b48
Greg Douglas <greg@webs.com>
2014-04-06 05:54:50 -0400

    Quick commit

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 
+var exec = require('child_process').exec;
+
 // include the express body parser
 app.configure(function () {
-  app.use(express.bodyParser());
+    //app.use(express.bodyParser());
+    app.use(express.bodyParser({ keepExtensions: true, uploadDir: __dirname + '/uploads/images' }));
 });
 
 var form = "<!DOCTYPE HTML><html><body>" +
@@ -14,8 +17,16 @@ var form = "<!DOCTYPE HTML><html><body>" +
 
 app.get('/', function (req, res){
 	res.writeHead(200, {'Content-Type': 'text/html' });
-	res.end(form);	
+	res.end(form);
 });
+
+var users = {
+
+    'alice': { snaps: [{from: 'alex', url: 'blah.com'}]},
+    'alex': { snaps: []},
+    'greg': { snaps: []},
+    'ilya': { snaps: []}
+};
 
 // include the node file module
 var fs = require('fs');
@@ -26,13 +37,20 @@ var VIDEOS_DIR = "/uploads/videos/";
 // post some files
 app.post('/upload', function(req, res) {
 
+
 	fs.readFile(req.files.image.path, function (err, data) {
 
-		var imageName = req.files.image.name
+        // console.log(req.files.image.name);
+
+
+		var imageName = req.files.image.name;
+
+        var from_user = "greg";
+        var to_user = "alice";
 
 		// if there's an error
 		if (!imageName) {
-			console.log("There was an error")
+			console.log("There was an error");
 			res.redirect("/");
 			res.end();
 		} else {
@@ -43,10 +61,42 @@ app.post('/upload', function(req, res) {
             // let's see it
 			fs.writeFile(newPath, data, function (err) {
 
-				res.redirect(VIDEOS_DIR + videoName);
+
+                users[to_user]['snaps'].push({from: from_user, url: VIDEOS_DIR + videoName});
+                console.log("Sent: " + VIDEOS_DIR + videoName +
+                    " From: " + from_user +
+                    " To: " + to_user);
+                res.end('success');
+
+
+
+//                exec('java Test', function callback(error, stdout, stderr){
+//
+//                    console.log(stdout);
+//
+//
+//                    // res.redirect(VIDEOS_DIR + videoName);
+//                    res.end('success');
+//                });
 			});
 		}
 	});
+});
+
+app.get('/users/:name', function (req, res){
+    res.writeHead(200, {"Content-Type": "application/json"});
+
+    var user = users[req.params.name];
+
+    console.log(user);
+
+    var snaps = user['snaps'];
+
+    if (user == undefined) {
+        res.end("{'error':'not a user'}");
+    } else {
+        res.end(JSON.stringify(snaps));
+    }
 });
 
 // show some images
@@ -58,7 +108,7 @@ app.get(IMAGES_DIR + ':file', function (req, res){
 });
 
 // show some videos
-app.get(VIDEOS_DIR + '/uploads/videos/:file', function (req, res){
+app.get(VIDEOS_DIR + ':file', function (req, res){
 	var file = req.params.file;
 	var img = fs.readFileSync(__dirname + VIDEOS_DIR + file);
 	res.writeHead(200, {'Content-Type': 'video/mp4' });
